62a1f5fe56bf7226b4f2338039faa7b8fa38ae4b
liyangyang <1054949480@qq.com>
2020-06-23 19:54:11 +0800

    master

--- src/main/java/com/example/lyy/demo/Test4.java
+++ src/main/java/com/example/lyy/demo/Test4.java
@@ -1,5 +1,6 @@
 package com.example.lyy.demo;
 
+import com.google.common.base.Converter;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -9,11 +10,14 @@ import java.util.List;
 
 public class Test4 {
     public static void main(String[] args) throws IOException {
-        List<String> list = FileUtils.readLines(new File("D:\\888.txt"));
-        int i = 0;
-        for (String url : list) {
-            FileUtils.copyURLToFile(new URL(url),new File("E:\\image\\"+ i + ".jpg"));
-            i++;
-        }
+        System.out.println(2218325903732378824L >> 10);
+        System.out.println(2166333890363651l << 10);
+
+//        List<String> list = FileUtils.readLines(new File("D:\\888.txt"));
+//        int i = 0;
+//        for (String url : list) {
+//            FileUtils.copyURLToFile(new URL(url),new File("E:\\image\\"+ i + ".jpg"));
+//            i++;
+//        }
     }
 }
