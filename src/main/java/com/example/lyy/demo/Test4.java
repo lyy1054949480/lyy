a18d829437b68245b7f5540f155e2dcc1e79a650
liyangyang <1054949480@qq.com>
2020-06-24 15:21:13 +0800

    java自省

--- src/main/java/com/example/lyy/demo/Test4.java
+++ src/main/java/com/example/lyy/demo/Test4.java
@@ -9,18 +9,29 @@ import org.springframework.beans.BeanInfoFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.ReflectionUtils;
 
-import java.beans.BeanInfo;
-import java.beans.Introspector;
+import java.beans.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.List;
 
 public class Test4 {
-    public static void main(String[] args) throws IOException {
-        System.out.println(2218325903732378824L >> 10);
-        System.out.println(2166333890363651l << 10);
-
+    public static void main(String[] args) throws Exception {
+        Image image = Image.builder().name("1111").build();
+        PropertyDescriptor descriptor = new PropertyDescriptor("name", image.getClass());
+        //字段名
+        System.out.println(descriptor.getName());
+        //字段类型
+        System.out.println(descriptor.getPropertyType().getName());
+        //get
+        Method readMethod = descriptor.getReadMethod();
+        Object invoke = readMethod.invoke(image);
+        System.out.println(invoke);
+        //set
+        Method writeMethod = descriptor.getWriteMethod();
+        Object invoke1 = writeMethod.invoke(image, "2222");
+        System.out.println(image.getName());
 //        List<String> list = FileUtils.readLines(new File("D:\\888.txt"));
 //        int i = 0;
 //        for (String url : list) {
