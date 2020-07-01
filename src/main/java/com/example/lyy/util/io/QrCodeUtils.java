5e676c3c658d01c35fe61048fc19ec3f6692575b
liyangyang <1054949480@qq.com>
2020-07-01 14:09:04 +0800

    qrcode

--- src/main/java/com/example/lyy/util/io/QrCodeUtils.java
+++ src/main/java/com/example/lyy/util/io/QrCodeUtils.java
@@ -1,17 +1,14 @@
 package com.example.lyy.util.io;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
+import com.google.zxing.*;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Hashtable;
 
 /**
@@ -36,7 +33,8 @@ public class QrCodeUtils {
         Hashtable<EncodeHintType, Object> hints = new Hashtable<>(16);
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
         hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
-        hints.put(EncodeHintType.MARGIN, 1);
+        //设置条码两边空白边距为1，默认为10，如果宽度不是条码自动生成宽度的倍数则MARGIN无效
+        hints.put(EncodeHintType.MARGIN, 0);
         BitMatrix bitMatrix = null;
         try {
             bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, qrWidth, qrHeight,
@@ -73,9 +71,33 @@ public class QrCodeUtils {
         }
     }
 
-    public static void main(String[] args) {
-//        String url = basePath +"?id="+id;
-//        BufferedImage image = QrCodeUtils.createCode(url);
-//        QrCodeUtils.codeOutput(response, image);
+    /**
+     * 解析条形码
+     *
+     * @param imgPath
+     * @return
+     */
+    public static String decode(String imgPath) {
+        BufferedImage image = null;
+        Result result = null;
+        try {
+            image = ImageIO.read(new File(imgPath));
+            if (image == null) {
+                throw new RuntimeException("the decode image may be not exists.");
+            }
+            LuminanceSource source = new BufferedImageLuminanceSource(image);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            result = new MultiFormatReader().decode(bitmap, null);
+            return result.getText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = "qqqqqqqqqqqqqqqqqqqqqqqq";
+        BufferedImage image = QrCodeUtils.createCode(url);
+        ImageIO.write(image, FORMAT_NAME, new FileOutputStream(new File("D:\\1111.jpg")));
     }
 }
