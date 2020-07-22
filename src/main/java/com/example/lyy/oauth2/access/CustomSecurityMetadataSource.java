864316788733b28c2b4f8990c8da319f6e00eb84
liyangyang <1054949480@qq.com>
2020-07-22 17:18:17 +0800

    Revert "注释security相关"
    This reverts commit 978eaf60d8eea758ae0c539fd929611e5618162e.

--- src/main/java/com/example/lyy/oauth2/access/CustomSecurityMetadataSource.java
+++ src/main/java/com/example/lyy/oauth2/access/CustomSecurityMetadataSource.java
@@ -1,41 +1,41 @@
-//package com.example.lyy.oauth2.access;
-//
-//import org.springframework.security.access.ConfigAttribute;
-//import org.springframework.security.web.FilterInvocation;
-//import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
-//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//import org.springframework.security.web.util.matcher.RequestMatcher;
-//
-//import java.util.Collection;
-//import java.util.Map;
-//
-//public class CustomSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
-//
-//
-//    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
-//        FilterInvocation fi = (FilterInvocation) object;
-//        Map<String, Collection<ConfigAttribute>> metadataSource = null;
-//        try {
-//            metadataSource = CustomSecurityContext.getMetadataSource();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        for (Map.Entry<String, Collection<ConfigAttribute>> entry : metadataSource.entrySet()) {
-//            String uri = entry.getKey();
-//            RequestMatcher requestMatcher = new AntPathRequestMatcher(uri);
-//            if (requestMatcher.matches(fi.getHttpRequest())) {
-//                return entry.getValue();
-//            }
-//        }
-//
-//        return null;
-//    }
-//
-//    public Collection<ConfigAttribute> getAllConfigAttributes() {
-//        return null;
-//    }
-//
-//    public boolean supports(Class<?> clazz) {
-//        return FilterInvocation.class.isAssignableFrom(clazz);
-//    }
-//}
+package com.example.lyy.oauth2.access;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        FilterInvocation fi = (FilterInvocation) object;
+        Map<String, Collection<ConfigAttribute>> metadataSource = null;
+        try {
+            metadataSource = CustomSecurityContext.getMetadataSource();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (Map.Entry<String, Collection<ConfigAttribute>> entry : metadataSource.entrySet()) {
+            String uri = entry.getKey();
+            RequestMatcher requestMatcher = new AntPathRequestMatcher(uri);
+            if (requestMatcher.matches(fi.getHttpRequest())) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        return null;
+    }
+
+    public boolean supports(Class<?> clazz) {
+        return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+}
